9fc91b2a7c524c6e4f154312fd66f0886d051b98
admin <123456>
2019-05-03 23:03:17 +0800

    Initial commit

---
+++ src/main/java/org/mif/serial/monitor/vo/EquipmentVO.java
@@ -0,0 +1,15 @@
+package org.mif.serial.monitor.vo;
+
+import lombok.Data;
+
+/**
+ * @description: 设备信息
+ * @author: mif
+ * @date: 2019/5/3 22:16
+ */
+@Data
+public class EquipmentVO  {
+
+    private String equipmentNo;
+
+}
